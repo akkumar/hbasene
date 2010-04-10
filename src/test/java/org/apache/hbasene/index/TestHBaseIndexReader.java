5b657dcb790e10561a7875fbad63c2eec7834dcb
Karthik K <oss.akk@gmail.com>
2010-04-10 01:07:49 -0700

    initial test case for index searcher

---
+++ src/test/java/org/apache/hbasene/index/TestHBaseIndexReader.java
@@ -0,0 +1,43 @@
+package org.apache.hbasene.index;
+
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TopDocs;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestHBaseIndexReader extends AbstractHBaseneTest {
+
+  private static IndexReader reader;
+
+  @BeforeClass
+  public static void setUp() {
+    reader = new HBaseIndexReader(conf, TEST_INDEX);
+  }
+
+  @AfterClass
+  public static void tearDown() throws IOException {
+    reader.close();
+  }
+
+  @Test
+  public void testSearch() throws IOException {
+    IndexSearcher searcher = new IndexSearcher(reader);
+    TopDocs docs = searcher.search(new TermQuery(new Term("content", "plays")),
+        3);
+    Assert.assertTrue("At least 3 terms with the keyword plays available",
+        docs.totalHits > 3);
+    for (ScoreDoc doc : docs.scoreDocs) {
+      Assert.assertTrue(doc.doc > 0); // valid docId
+      // Assert.assertTrue(doc.score > 0.0f); // valid Score
+    }
+  }
+}
