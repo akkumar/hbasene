84154859e63ed9d26a74a31fc7248dc12c93bad9
Karthik K <oss.akk@gmail.com>
2010-04-09 18:17:36 -0700

    Add licensing headers

--- src/main/java/org/apache/nosql/lucene/index/NoSqlIndexWriter.java
+++ src/main/java/org/apache/nosql/lucene/index/NoSqlIndexWriter.java
@@ -1,6 +1,9 @@
 /**
  * Copyright 2010 Karthik Kumar
  * 
+ * Based off the original code by Lucandra project, (C): Jake Luciani
+ * 
+ * 
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
