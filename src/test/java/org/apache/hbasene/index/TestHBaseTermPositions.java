93b57dfeb92d76874609b90562f13a0b54f5454c
Karthik K <oss.akk@gmail.com>
2010-04-10 23:37:37 -0700

    add more test cases

--- src/test/java/org/apache/hbasene/index/TestHBaseTermPositions.java
+++ src/test/java/org/apache/hbasene/index/TestHBaseTermPositions.java
@@ -25,10 +25,10 @@ import java.io.IOException;
 import junit.framework.Assert;
 
 import org.apache.log4j.Logger;
-import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.Term;
-import org.apache.lucene.store.LockObtainFailedException;
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -38,15 +38,16 @@ public class TestHBaseTermPositions extends AbstractHBaseneTest {
   private static final Logger LOGGER = Logger
       .getLogger(TestHBaseTermPositions.class.getName());
 
-  private static HBaseTermPositions termPositions;
+  private static HBaseIndexReader indexReader;
+
+  private HBaseTermPositions termPositions;
 
   /**
    * @throws java.lang.Exception
    */
   @BeforeClass
   public static void setUp() throws Exception {
-    HBaseIndexReader indexReader = new HBaseIndexReader(conf, TEST_INDEX);
-    termPositions = new HBaseTermPositions(indexReader);
+    indexReader = new HBaseIndexReader(conf, TEST_INDEX);
 
   }
 
@@ -55,12 +56,22 @@ public class TestHBaseTermPositions extends AbstractHBaseneTest {
    */
   @AfterClass
   public static void tearDown() throws Exception {
+    indexReader.close();
+  }
+
+  @Before
+  public void beforeTest() throws IOException {
+    termPositions = new HBaseTermPositions(indexReader);
+
+  }
+
+  @After
+  public void afterTest() throws IOException {
     termPositions.close();
   }
 
   @Test
-  public void testTermDocs() throws CorruptIndexException,
-      LockObtainFailedException, IOException {
+  public void testTermDocs() throws IOException {
     termPositions.seek(new Term("content", "plays"));
     int count = 0;
     while (termPositions.next()) {
@@ -69,6 +80,58 @@ public class TestHBaseTermPositions extends AbstractHBaseneTest {
       ++count;
     }
     Assert.assertEquals("plays occurs 4 ", 4, count);
+  }
+
+  @Test
+  public void testReadNormal() throws IOException {
+    int[] docs = new int[4];
+    int[] freqs = new int[4];
+    termPositions.seek(new Term("content", "plays"));
+    Assert.assertEquals(4, termPositions.read(docs, freqs));
+  }
+
+  @Test
+  public void testReadOverflowDocs() throws IOException {
+    int[] docs = new int[32];
+    int[] freqs = new int[32];
+    termPositions.seek(new Term("content", "plays"));
+    Assert.assertEquals(4, termPositions.read(docs, freqs));
+  }
+
+  @Test
+  public void testReadUnderflowDocs() throws IOException {
+    int[] docs = new int[3];
+    int[] freqs = new int[3];
+    termPositions.seek(new Term("content", "plays"));
+    Assert.assertEquals(3, termPositions.read(docs, freqs));
+  }
+
+  @Test
+  public void testReadMultipleSplits() throws IOException {
+    termPositions.seek(new Term("content", "plays"));
+    int[] docs = new int[3];
+    int[] freqs = new int[3];
+    Assert.assertEquals(3, termPositions.read(docs, freqs));
+
+    docs = new int[1];
+    freqs = new int[1];
+    Assert.assertEquals(1, termPositions.read(docs, freqs));
 
   }
+
+  @Test
+  public void testReadAtLastBoundary() throws IOException {
+    termPositions.seek(new Term("content", "plays"));
+    int[] docs = new int[3];
+    int[] freqs = new int[3];
+    Assert.assertEquals(3, termPositions.read(docs, freqs));
+
+    docs = new int[1];
+    freqs = new int[1];
+    Assert.assertEquals(1, termPositions.read(docs, freqs));
+
+    Assert.assertEquals(0, termPositions.read(docs, freqs));
+
+  }
+
 }
