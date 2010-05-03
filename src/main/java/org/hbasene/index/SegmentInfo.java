0df28b283bcf5829206f4940fd37389d349ce820
Karthik K <oss.akk@gmail.com>
2010-05-03 12:54:59 -0700

    initial version

---
+++ src/main/java/org/hbasene/index/SegmentInfo.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2010 Karthik Kumar
+ * 
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * 
+ */
+package org.hbasene.index;
+
+public class SegmentInfo {
+
+  private final int documentId;
+  
+  private final long segmentId;
+  
+  public SegmentInfo( final long segmentId, final int documentId) { 
+    this.documentId = documentId;
+    this.segmentId = segmentId;
+  }
+  
+  public long getSegmentId() { 
+    return this.segmentId;
+  }
+  
+  public int getDocumentId() { 
+    return this.documentId;
+  }
+}
